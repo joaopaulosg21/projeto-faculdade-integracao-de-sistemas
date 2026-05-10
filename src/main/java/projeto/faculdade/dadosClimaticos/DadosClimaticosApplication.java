acb740536f9d048b3fd86b047b813357452f8884
joaopaulosg <joaopaulosg17@gmail.com>
2026-05-10 14:39:59 -0300

    feat: Primeiro commit

---
+++ src/main/java/projeto/faculdade/dadosClimaticos/DadosClimaticosApplication.java
@@ -0,0 +1,13 @@
+package projeto.faculdade.dadosClimaticos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadosClimaticosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadosClimaticosApplication.class, args);
+	}
+
+}
